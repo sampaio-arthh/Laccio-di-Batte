c97d1fe897eaf3be20d3f4f91f47247191484d2c
Artheir <sampaio.thpp@gmail.com>
2023-11-08 12:40:10 -0300

    08.11 Poucas mudanças, mais a serem feitas

--- salvalia.php
+++ salvalia.php
@@ -28,22 +28,22 @@ if(!mysqli_select_db($c,$banco))
 $ans = mysqli_query($c, "SELECT id_aval from avaliacao where id_aval = $id_avaliacao");
 if (!$ans) {
 	$sql = "insert into avaliacao(id_aval,tempo,sabor,sistema) values ('$id_avaliacao','$tempo','$sabor','$sistema')";
+	$resp = mysqli_query($c, $sql);
+
+	if(!$resp){
+		echo "Desculpe, erro na consulta $sql";
+		echo mysqli_error($c);
+		mysqli_close($c);
+		die();
+	}
+	else{
+		header("location: index.php");
+	}
 }
 else {
-	echo "<script>alert(Número de avaliação já utilizado ! Para editar vá na página correta)</script>";
-	header("location: index.php");
+	header("location: invalid.php");
 }
 
-$resp = mysqli_query($c, $sql);
 
-if(!$resp){
-	echo "Desculpe, erro na consulta $sql";
-	echo mysqli_error($c);
-	mysqli_close($c);
-	die();
-}
-else{
-	header("location: index.php");
-}
 
 ?>
